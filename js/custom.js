b9f4649b77320c7b7cff3345c82daab2bf810e2c
Ian Petruk <Ian_Petruk@epam.com>
2020-03-26 19:11:41 +0300

    Hometask 3.

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,74 @@
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
   });
+
+  let timer = document.getElementById("timer");
+  let minute = 0;
+
+  function tick() {
+    minute++;
+    const result = `You are here for ${minute} minute`;
+
+    minute !== 1
+      ? (timer.innerHTML = result + "s")
+      : (timer.innerHTML = result);
+  }
+
+  setInterval(() => tick(), 60000);
+
+  let skipNavLink = document.getElementById("skip-nav-link");
+  let currentSkipNavLinkItem = 0;
+
+  function goDown() {
+    event.preventDefault();
+    if (currentSkipNavLinkItem === 2) {
+      return skipNavLink.children[2].setAttribute("tabindex", "0");
+    }
+
+    ++currentSkipNavLinkItem;
+    skipNavLink.children[currentSkipNavLinkItem].setAttribute("tabindex", "0");
+    skipNavLink.children[currentSkipNavLinkItem].focus();
+    skipNavLink.children[currentSkipNavLinkItem - 1].setAttribute(
+      "tabindex",
+      "-1"
+    );
+  }
+
+  function goUp() {
+    event.preventDefault();
+    if (currentSkipNavLinkItem === 0) {
+      return skipNavLink.children[0].setAttribute("tabindex", "0");
+    }
+
+    --currentSkipNavLinkItem;
+    skipNavLink.children[currentSkipNavLinkItem].setAttribute("tabindex", "0");
+    skipNavLink.children[currentSkipNavLinkItem].focus();
+    skipNavLink.children[currentSkipNavLinkItem + 1].setAttribute(
+      "tabindex",
+      "-1"
+    );
+  }
+
+  function jumpTo() {
+    skipNavLink.children[currentSkipNavLinkItem].children[0].click();
+  }
+
+  function keyboardNavigation(e) {
+    switch (e.code) {
+      case "ArrowUp":
+        goUp();
+        break;
+      case "ArrowDown":
+        goDown();
+        break;
+      case "Enter":
+      case "Space":
+        jumpTo();
+        break;
+    }
+  }
+
+  skipNavLink.onkeydown = keyboardNavigation;
 })();
 
 document.querySelectorAll("#nav li").forEach(function(navEl) {
